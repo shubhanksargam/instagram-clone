6867c08ffc063a3f6fcc6a51a340d309d3bea35d
Shubhankoder <54004976+Shubhankoder@users.noreply.github.com>
2022-06-20 13:09:46 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import Post from './Post';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+        <div className="app__header">
+        <img 
+              src="https://www.instagram.com/static/images/web/mobile_nav_type_logo.png/735145cfe0a4.png"
+              alt=""
+              className="app__header-image" />
+
+        </div>
+              <h1>HELLO Guys Let's build an instagram clone with React 🚀 </h1>
+        {/* Header */}
+
+        {/* Posts */}
+        {/* Posts */}
+
+        <Post />
+
+
     </div>
   );
 }
---
+++ src/Post.js
@@ -0,0 +1,17 @@
+
+import React from 'react'
+
+function Post() {
+  return (
+    <div>
+        <h3>Username</h3>
+        {/*header -> avatar + username*/}
+        {/* image */}
+        {/* username + caption*/}
+
+        <img src="https://twitter.com/ShubhankSargam/photo" alt="" />
+        </div>
+  )
+}
+
+export default Post
